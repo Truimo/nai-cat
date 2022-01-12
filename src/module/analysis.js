d8a666af39f179bfef0824801fd3d9bf853307f8
Truimo <xiaomo@20mo.cn>
2022-01-12 13:59:17 +0800

    analysis

---
+++ src/module/analysis.js
@@ -0,0 +1,15 @@
+import htmlspecialchars from '@/module/htmlspecialchars'
+
+const analysis = (content) => {  // 解析
+    try {
+        content = htmlspecialchars(content)
+        content = content.replace(/\r\n/g, '<br/>')
+        content = content.replace(/\n/g, '<br/>')
+        content = content.replace(/\[CQ:.+\]/, '<i class="text-gray-400">NaN</i>')
+    } catch (e) {
+        console.log('解析出错')
+    }
+    return content
+}
+
+export default analysis
